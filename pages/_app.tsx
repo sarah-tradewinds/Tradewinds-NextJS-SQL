9631782445c5089b7f843521910f73c5e55c2291
vishal.singh4 <vishal.singh@ceptes.com>
2022-05-05 21:29:46 +0530

    added keen style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,13 @@ import Layout from 'components/website/common/layout';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
+
+// styles
+import 'keen-slider/keen-slider.min.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import '../styles/globals.css';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	const router = useRouter();
 
