a6fcaddd0bebdcc897909b276387b26da66e0805
vishal <contact@vishalsync.com>
2022-12-16 21:21:25 +0530

    Changed url for uat

--- next.config.js
+++ next.config.js
@@ -35,23 +35,23 @@ const nextConfig = (phase) => {
 		i18n,
 		reactStrictMode: true,
 		env: {
-			SITE_URL: 'https://tradewinds.vercel.app',
+			// SITE_URL: 'https://tradewinds.vercel.app',
 			// #DEV
 			// SITE_URL: 'https://tradewindsmpshoppingsite.azurewebsites.net',
 			// #UAT
-			// SITE_URL: 'https://tradewindsmpuatshoppingsite.azurewebsites.net',
+			SITE_URL: 'https://tradewindsmpuatshoppingsite.azurewebsites.net',
 
 			BUYER_DASHBOARD_SITE_URL:
-				'https://tradewinds-dev-public.s3.us-east-2.amazonaws.com/index.html#',
-			// #DEV
-			// 'www.tradewindsdev.com',
-			// #UAT
-			// 'https://www.tradewindsqa.com/#',
+				// 'https://tradewinds-dev-public.s3.us-east-2.amazonaws.com/index.html#',
+				// #DEV
+				// 'www.tradewindsdev.com',
+				// #UAT
+				'https://www.tradewindsqa.com/#',
 			API_BASE_URL:
 				// #DEV
-				'https://tradewinds-dev.eastus.cloudapp.azure.com',
-			// #UAT
-			// 'https://tradewinds-uat-api.azure-api.net',
+				// 'https://tradewinds-dev.eastus.cloudapp.azure.com',
+				// #UAT
+				'https://tradewinds-uat-api.azure-api.net',
 			STRIPE_PUBLISHABLE_KEY:
 				'pk_test_51JAhKFCQTcpcYTusSG85R25JWHRXIbw9mg6bn2mJAIncKhAhXpxvAemulgavKUw0iv4J1ygM6zSWGbthNAMWGftk00Jzzmiqcs'
 		},
