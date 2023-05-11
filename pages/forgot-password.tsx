19a3fffda0f6b324212cae5d8636e6dab38c8119
Basavaraja88 <basavaraja@synergytechs.net>
2023-05-11 19:21:36 +0530

    password

--- pages/forgot-password.tsx
+++ pages/forgot-password.tsx
@@ -3,12 +3,13 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useAuthStore } from 'store/auth';
 
-import Seo from 'components/common/seo';
-
 import { forgetPasswordChange } from 'components/common/auth/auth-services';
+import Button from 'components/common/form/button';
+import Seo from 'components/common/seo';
 import { forgetPasswordGenerateLink } from 'lib/customer/auth.lib';
-import { AiOutlineMail } from 'react-icons/ai';
+import { HiSparkles } from 'react-icons/hi';
 import { RiLockPasswordLine } from 'react-icons/ri';
+import ImageWithErrorHandler from '../src/components/common/elements/image-with-error-handler';
 
 const ForgotPassword = () => {
 	const authStore = useAuthStore();
@@ -178,99 +179,133 @@ const ForgotPassword = () => {
 	return (
 		<>
 			<Seo title="Forgot password page" description="" />
-			<div className="flex h-screen bg-primary-main">
+
+			<div className="flex h-screen bg-bg-main">
 				{!token && !status.isForgotEmailSent && (
-					<div className="border-primaryBorder shadow-default m-auto w-full max-w-md rounded-lg border bg-[#c7ecff] py-10 px-4">
-						<h1 className="mt-4 mb-4 text-center text-3xl font-bold">
-							Forgot password
-						</h1>
+					<div className=" m-auto">
+						<div className="flex justify-center rounded-md bg-white shadow-md md:mt-12 md:w-[740px] md:py-4 lg:w-[1000px] lg:justify-start lg:px-16">
+							<div className=" flex h-[640px] flex-col items-center overflow-auto border-gray/40 py-8 md:h-auto lg:w-full lg:border-r lg:py-0 lg:pr-24">
+								<div className=" my-auto">
+									<h2 className="mb-8 border-b border-gray/40 pb-4 text-center text-3xl font-semibold text-black md:text-4xl">
+										Forgot password
+									</h2>
 
-						<h1 className="mt-5 mb-2 font-bold">
-							Please enter the registered email address
-						</h1>
+									<div className="flex w-full justify-center border-b border-gray/40 pb-4">
+										<form className="space-y-4 px-8 lg:w-[360px] lg:px-0">
+											<h1>Please enter the registered email address</h1>
+											<Input
+												name="email"
+												type="email"
+												placeholder="Email*"
+												icon={<HiSparkles />}
+												className="w-full"
+												onChange={(
+													e: React.FormEvent<HTMLInputElement>
+												) => {
+													setEmail(e.currentTarget.value);
+													setStatus({ ...status, message: '' });
+												}}
+											/>
 
-						<Input
-							name="email"
-							type="email"
-							placeholder="Email*"
-							icon={<AiOutlineMail />}
-							className="w-full"
-							onChange={(e: React.FormEvent<HTMLInputElement>) => {
-								setEmail(e.currentTarget.value);
-								setStatus({ ...status, message: '' });
-							}}
-						/>
-
-						{status.message && !status.isForgotEmailSent && (
-							<div className=" mt-2 text-sm text-[red]">
-								{status.message}
+											<Button
+												variant="product"
+												className="w-full"
+												disabled={loading}
+												onClick={submitPasswordResetRequest}
+												type="button"
+											>
+												Submit
+											</Button>
+										</form>
+									</div>
+									<div className=" mt-2">
+										<span className="font-semibold">
+											Not a Buyer yet?
+										</span>
+										<span
+											className="ml-1 cursor-pointer underline"
+											onClick={() => {
+												authStore.setIsSignUpOpen();
+												router.push('/');
+											}}
+										>
+											Create your account now!
+										</span>
+									</div>
+								</div>
 							</div>
-						)}
 
-						<button
-							className="mt-3 rounded border border-[green] bg-[green] py-2 px-4 text-sm hover:bg-opacity-75 focus:outline-none disabled:opacity-60 "
-							disabled={(isValidEmail() ? false : true) || loading}
-							onClick={submitPasswordResetRequest}
-							type="button"
-						>
-							{loading && (
-								<svg
-									role="status"
-									className="mr-3 inline h-6 w-6 animate-spin text-[blue]"
-									viewBox="0 0 100 101"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-										fill="#E5E7EB"
+							{/* Logos */}
+							<div className="hidden w-full flex-col items-center justify-center lg:flex">
+								<div className="relative h-[260px] w-[260px]">
+									<ImageWithErrorHandler
+										src="/TW-Create an account page-02.png"
+										alt=""
+										fill={true}
+										className="object-contain"
 									/>
-									<path
-										d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-										fill="currentColor"
+								</div>
+
+								<div className="relative h-[260px] w-[260px]">
+									<ImageWithErrorHandler
+										src="/TW-Create an account page-03.png"
+										alt=""
+										fill={true}
+										className="object-contain"
 									/>
-								</svg>
-							)}
-							Submit
-						</button>
-
-						<hr className="mt-3 text-[#868686]" />
-
-						<div className="mt-4 mb-2 text-sm">
-							<span className="font-bold">Not a Buyer yet?</span>
-							<span
-								className="ml-1 cursor-pointer underline"
-								onClick={() => {
-									authStore.setIsSignUpOpen();
-									router.push('/');
-								}}
-							>
-								Create your account now!
-							</span>
+								</div>
+							</div>
 						</div>
 					</div>
 				)}
 
 				{!token && status.isForgotEmailSent && (
-					<div className="border-primaryBorder shadow-default m-auto w-full max-w-md rounded-lg border bg-[#c7ecff] py-10 px-5">
-						<h1 className="font-large text-1xl mt-4 mb-2 ">
-							An email has been sent to the email address provided (
-							{email}). Please check your email and follow the
-							instructions to reset your password.
-						</h1>
+					<div className=" m-auto">
+						<div className="flex justify-center rounded-md bg-white shadow-md md:mt-12 md:w-[740px] md:py-4 lg:w-[1000px] lg:justify-start lg:px-16">
+							<div className=" flex h-[640px] flex-col items-center overflow-auto border-gray/40 py-8 md:h-auto lg:w-full lg:border-r lg:py-0 lg:pr-24">
+								<div className="border-primaryBorder shadow-default m-auto w-full max-w-md rounded-lg border bg-white py-10 px-5">
+									<h1 className="font-large text-1xl mt-4 mb-2 ">
+										An email has been sent to the email address provided
+										({email}). Please check your email and follow the
+										instructions to reset your password.
+									</h1>
 
-						<hr className="mt-3 text-[#868686]" />
+									<hr className="mt-3 text-[#868686]" />
 
-						<div className="mt-4 mb-2">
-							<span
-								className="ml-1 cursor-pointer underline"
-								onClick={() => {
-									authStore.setIsLoginOpen();
-									router.push('/');
-								}}
-							>
-								Login to your account
-							</span>
+									<div className="mt-4 mb-2">
+										<span
+											className="ml-1 cursor-pointer underline"
+											onClick={() => {
+												authStore.setIsLoginOpen();
+												router.push('/');
+											}}
+										>
+											Login to your account
+										</span>
+									</div>
+								</div>
+							</div>
+
+							{/* Logos */}
+							<div className="hidden w-full flex-col items-center justify-center lg:flex">
+								<div className="relative h-[260px] w-[260px]">
+									<ImageWithErrorHandler
+										src="/TW-Create an account page-02.png"
+										alt=""
+										fill={true}
+										className="object-contain"
+									/>
+								</div>
+
+								<div className="relative h-[260px] w-[260px]">
+									<ImageWithErrorHandler
+										src="/TW-Create an account page-03.png"
+										alt=""
+										fill={true}
+										className="object-contain"
+									/>
+								</div>
+							</div>
 						</div>
 					</div>
 				)}
