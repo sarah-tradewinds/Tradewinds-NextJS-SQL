b53d899092a3f122cb54d27ed068ab3e8b695ef8
Basavaraja88 <basavaraja@synergytechs.net>
2023-07-10 00:05:36 +0530

    whybuychangeseco

--- src/components/common/rfq/point.tsx
+++ src/components/common/rfq/point.tsx
@@ -7,7 +7,7 @@ const Point: React.FC<PointProps> = (props) => {
 	return (
 		<div>
 			<div className="flexitems-center h-[10px] w-[20px] justify-center  ">
-				<div className="rotate-45 transform text-4xl sm:pr-[40px] md:pr-[40px]  lg:pr-[40px] desktop:pr-[30px]">
+				<div className="rotate-45 transform pr-[40px] text-4xl sm:pr-[40px] md:pr-[40px]  lg:pr-[40px] desktop:pr-[30px]">
 					.
 				</div>
 			</div>
